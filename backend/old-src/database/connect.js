3c0256f963006ed9f2a2eaea863436da9a675fc3
Victor Rubens <victorrub2013@outlook.com>
2020-08-12 00:22:28 -0300

    feat : cleaning project, removing old files that have been refactored

--- backend/old-src/database/connect.js
+++
@@ -1,7 +0,0 @@
-import knex from "knex";
-import * as config from "../../knexfile";
-
-const env = process.env.NODE_ENV || "development";
-const connect = knex(config[env]);
-
-export default connect;
